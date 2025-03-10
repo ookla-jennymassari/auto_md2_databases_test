1a69d9155fc84b7ce25acf1646695d264933b9e2
Jenny Massari <jennymassari@ookla-mac-jennymassari.local>
2025-03-09 20:52:10 -0700

    Added SQL queries testing  clauses.

--- atlanta_set_test/atlanta_set_test.py
+++ atlanta_set_test/atlanta_set_test.py
@@ -23,4 +23,52 @@ except Exception as e:
 df_atlanta_sql = pd.read_sql("select * from auto.test_summary_reporting where collection_set_id = 12319;",
                            con=os.getenv('RSR_SVC_CONN'))
 
-print(df_atlanta_sql)
+# print(df_atlanta_sql)
+
+#Using AND/ OR operator to restrict the results to a smaller set
+df_atlanta_sql_and_operator = pd.read_sql("select * from auto.test_summary_reporting where collection_set_id = 12319 and carrier = 'Verizon'; ",
+                           con=os.getenv('RSR_SVC_CONN'))
+
+# print(df_atlanta_sql_and_operator)
+
+df_atlanta_sql_or_operator = pd.read_sql("select * from auto.test_summary_reporting where collection_set_id = 12319 and carrier = 'Verizon' or carrier = 'T-Mobile'; ",
+                           con=os.getenv('RSR_SVC_CONN'))
+# print(df_atlanta_sql_or_operator)
+
+#Using NOT logical operator
+# df_atlanta_sql_not_operator = pd.read_sql("select * from auto.test_summary_reporting where not collection_set_id = 12319 and carrier = 'Verizon'; ",
+#                            con=os.getenv('RSR_SVC_CONN'))
+# print(df_atlanta_sql_not_operator)
+
+# Ordering a result alphabetically by a specific field
+
+df_atlanta_sql_ordered = pd.read_sql("select * from auto.test_summary_reporting where collection_set_id = 12319 order by carrier;",
+                           con=os.getenv('RSR_SVC_CONN'))
+# print(df_atlanta_sql_ordered)
+
+# By default the numerically order is ascending to change the order to be descending:
+
+df_atlanta_sql_descending = pd.read_sql("select * from auto.test_summary_reporting where collection_set_id = 12319 order by test_event_id desc;",
+                           con=os.getenv('RSR_SVC_CONN'))
+
+# print(df_atlanta_sql_descending)
+
+#Order rows by NULL value
+df_atlanta_sql_null = pd.read_sql("select * from auto.test_summary_reporting where collection_set_id = 12319 order by access_success nulls first;",
+                           con=os.getenv('RSR_SVC_CONN'))
+# print(df_atlanta_sql_null)
+
+#Limit the numbers of records to retrieve from a table
+df_atlanta_sql_limited_by = pd.read_sql("select * from auto.test_summary_reporting where collection_set_id = 12319 limit 5;",
+                           con=os.getenv('RSR_SVC_CONN'))
+print(df_atlanta_sql_limited_by)
+
+# Skip a number of rows, start after that offset
+df_atlanta_sql_offset = pd.read_sql("select * from auto.test_summary_reporting where collection_set_id = 12319 offset 5;",
+                           con=os.getenv('RSR_SVC_CONN'))
+print(df_atlanta_sql_offset)
+
+#Retrieving the data from specific columns
+df_atlanta_sql_columns= pd.read_sql("select carrier, carrier_id from auto.test_summary_reporting where collection_set_id = 12319 limit 2;",
+                           con=os.getenv('RSR_SVC_CONN'))
+print(df_atlanta_sql_columns)
